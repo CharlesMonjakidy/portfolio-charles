25139f32e0796b3b286d803c52064ac070f2a65d
Charles Monjakidy <charlesmonjakidy@gmail.com>
2026-07-05 22:38:13 +0200

    ajout du skill

---
+++ script.js
@@ -0,0 +1,9 @@
+function hamburg(){
+    console.log("bouton ne marche plus")
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
