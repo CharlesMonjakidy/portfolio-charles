8282c92369b2cb7ce387973e104140720342eda9
Charles Monjakidy <charlesmonjakidy@gmail.com>
2026-07-11 18:48:38 +0200

    Régleage du navbar hamburg

--- script.js
+++
@@ -1,8 +0,0 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
-}
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
-}
